2fc42b5903b3088756af6c60cdb5f56165009bcd
Joao <joaorafael.leite@hotmail.com>
2023-12-26 08:06:57 -0300

    SellerService and FindAll

--- SalesWebMVC/Program.cs
+++ SalesWebMVC/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using SalesWebMVC.Data;
+using SalesWebMVC.Services;
 using static System.Formats.Asn1.AsnWriter;
 var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<SalesWebMVCContext>(options =>
@@ -9,6 +10,7 @@ builder.Services.AddDbContext<SalesWebMVCContext>(options =>
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddScoped<SeedingService>();
+builder.Services.AddScoped<SellerService>();
 
 var app = builder.Build();
 
